c10dcc68d206a5a7cf509aea448423719094cc51
Adib-Legastelois <adib.legastelois@icloud.com>
2020-07-22 17:37:32 +0100

    first commit

---
+++ bilingual-app/resources/views/pages/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <h1>This is bilingual French-English application.</h1>
+
+        <p>By continuing to browse this site, you consent to our use of cookies to improve your online experience. <a href="#">Learn more</a><button type="button"></button>OK</p>
+    </div>
+@endsection
